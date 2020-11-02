e9b5153d0fddf0e9b35eab5463998edb37c21d26
Sajra Turko <sturko@etf.unsa.ba>
2020-11-02 16:19:56 +0100

    vecina metoda dodana u klasu Interval

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++
@@ -1,6 +0,0 @@
-package ba.unsa.etf.rpr.tutorijal02;
-
-public class Interval {
-
-
-}
--- src/ba/unsa/etf/rpr/tutorijal02/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Main.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.rpr.tutorijal02;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Nema ništa u mainu, pokrenite testove!");
+        Interval i = new Interval();
+        
     }
 }
